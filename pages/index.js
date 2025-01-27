a0b3469e289bd3030d0877539e1c35670f5f0281
Samuel Hernandez <71301115+elsamird003@users.noreply.github.com>
2025-01-27 15:27:58 -0500

    Cleaning the webpage

--- pages/index.js
+++ pages/index.js
@@ -3,57 +3,31 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Spline from '@splinetool/react-spline';
 
+
 export default function Home() {
   return (
     <div className={styles.container}>
       
       <main>
-          <div className={styles.spline}>
-        <Spline
+        
+        {/* <Spline
           scene="https://prod.spline.design/vXgv9Ak06-gseQg0/scene.splinecode"
-        />
-        </div>
+        /> */}
+   
         <h1 className={styles.title}>
           Hi I'm Samuel Hernandez</h1>
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              all the work got deleted
-            </p>
-          </a>
+
         </div>
       </main>
      
       <footer>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://threejs.org/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+          Powered by Three.JS
         </a>
       </footer>
 
@@ -69,7 +43,7 @@ export default function Home() {
         footer {
           width: 100%;
           height: 100px;
-          border-top: 1px solid #eaeaea;
+          border-top: 1px solidrgb(161, 0, 0);
           display: flex;
           justify-content: center;
           align-items: center;
@@ -85,7 +59,7 @@ export default function Home() {
           color: inherit;
         }
         code {
-          background: #fafafa;
+          background:rgb(160, 0, 0);
           border-radius: 5px;
           padding: 0.75rem;
           font-size: 1.1rem;
